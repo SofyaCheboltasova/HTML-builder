228b77179fb7a7df6ea819044264321790d33961
Sofya <s.cheboltasova@g.nsu.ru>
2024-01-23 01:50:34 +0700

    refactor: update names for  functions and variables

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 const styleDirPath = path.join(__dirname, 'styles');
-const bundleFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const bundleDirPath = path.join(__dirname, 'project-dist');
 
 async function cleanUpBundleFile() {
+  const bundleFilePath = path.join(bundleDirPath, 'bundle.css');
   await fsPromises
     .truncate(bundleFilePath)
     .then(() => console.log('bundle.css successfully cleaned up\n'));
@@ -16,30 +17,40 @@ async function readStyleDirectory() {
   });
 }
 
-function isCssFile(file) {
-  const ext = path.extname(file.name).slice(1);
-  return ext === 'css';
+function hasExt(file, ext) {
+  const fileExt = path.extname(file.name).slice(1);
+  return ext === fileExt;
 }
 
-function fillBundleFile(file) {
-  const input = fs.createReadStream(
-    path.join(styleDirPath, file.name),
-    'utf-8',
-  );
-  const output = fs.createWriteStream(bundleFilePath, {
+function writeFile(paths) {
+  const { source, dest } = paths;
+  const input = fs.createReadStream(source, 'utf-8');
+  const output = fs.createWriteStream(dest, {
     flags: 'a',
     encoding: 'utf-8',
   });
-
   input.pipe(output);
-  console.log(`File ${file.name} merged to bundle.css\n`);
+}
+
+function getPathsObject(sourcePath, destPath, sourceFile, destFile) {
+  return {
+    source: path.join(sourcePath, sourceFile),
+    dest: path.join(destPath, destFile),
+  };
 }
 
 function handleStyleFiles() {
   readStyleDirectory().then((files) => {
     for (let file of files) {
-      if (!isCssFile(file)) continue;
-      fillBundleFile(file);
+      if (!hasExt(file, 'css')) continue;
+      const paths = getPathsObject(
+        styleDirPath,
+        bundleDirPath,
+        file.name,
+        'bundle.css',
+      );
+      writeFile(paths);
+      console.log(`File ${file.name} merged to bundle.css\n`);
     }
   });
 }
